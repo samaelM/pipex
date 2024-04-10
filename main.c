14f498ca2a47b0910c85f59374946697f406e00e
Mael Maldonado <maemaldo@student.42.fr>
2024-04-10 16:39:17 +0200

    add some things

--- main.c
+++ main.c
@@ -6,11 +6,30 @@
 /*   By: maemaldo <maemaldo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 18:21:11 by maemaldo          #+#    #+#             */
-/*   Updated: 2024/04/09 17:47:08 by maemaldo         ###   ########.fr       */
+/*   Updated: 2024/04/10 13:33:05 by maemaldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+char *ft_get_cmd_path(char **paths, char *cmd)
+{
+	int i;
+	char *tmp;
+	char *res;
+	i=0;
+	while (paths[i])
+	{
+		tmp = ft_strjoin(paths[i], "/");
+		res = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (access(res, F_OK))
+			return (res);
+		free(res);
+		i++;
+	}
+	return (cmd);
+}
 
 char	*ft_find_path(char **env)
 {
@@ -28,44 +47,15 @@ void	ft_err_exit(char *msg)
 	exit(0);
 }
 
-void ft_child1(t_pipex *pipex, char *argv[],char *envp[])
+void	ft_child1(t_pipex pipex, char *argv[], char *envp[])
 {
-	dup2(pipex->tube[1],1);
-	close(pipex->tube[0]);
-	dup2(pipex->infile,1);
+	dup2(pipex.tube[1], 1);
+	close(pipex.tube[0]);
+	dup2(pipex.infile, 1);
+	pipex.cmd_args = ft_split(argv[2], ' ');
+	pipex.cmd = ft_get_cmd_path(pipex.paths, pipex.cmd_args[0]);
+	execve(pipex.cmd, argv, envp);
 }
-
-// int	ft_len_first_word(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (str[i] && str[i] != ' ')
-// 		i++;
-// 	return (i);
-// }
-// int ft_copy_first_word(char *str, int len)
-// {
-
-// }
-// int	ft_conv_cmd(char *str)
-// {
-// 	char	*cmd;
-// 	char	len;
-// 	char	*args;
-// 	int		i;
-
-// 	len = ft_len_first_word(str);
-// 	cmd = malloc(sizeof(char) * len);
-// 	i = 0;
-// 	while (str[i] && str[i] != ' ')
-// 	{
-// 		cmd[i] = str[i];
-// 		i++;
-// 	}
-// 	cmd[i] = 0;
-// 	return (0);
-// }
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -73,11 +63,6 @@ int	main(int argc, char **argv, char **envp)
 	char	*cmd1;
 	char	*cmd2;
 
-	// char	*arg[] = {NULL};
-	// int		id;
-	// char	*outfile;
-	// char	*test[] = {};
-	// int		i;
 	if (argc != 5)
 		return (1);
 	pipex.infile = open(argv[1], O_RDONLY);
@@ -93,14 +78,6 @@ int	main(int argc, char **argv, char **envp)
 	pipex.pid1 = fork();
 	cmd2 = argv[3];
 	cmd1 = argv[2];
-	printf("cmd1 = %s\n", cmd1);
-	// i = 0;
-	// while (envp[i])
-	// 	printf("env = %s\n", envp[i++]);
-	// printf("fork\n");
-	// if (id != 0)
-	// 	(printf("premier process\n"), execve(cmd1, arg, envp), wait(NULL));
-	// else
-	// 	printf("deuxiemme process\n");
+	// printf("cmd1 = %s\n", cmd1);
 	return (0);
 }
